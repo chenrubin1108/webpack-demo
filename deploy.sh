662684cff09fc23bc39adfb21e2620a7b1f28e15
chen <154724313@qq.com>
2021-02-19 18:37:06 +0800

    deploy add

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 yarn build &&
 git checkout gh-pages &&
-
+rm -rf *.html *.json *.css *.json *.png &&
 mv dist/* ./ &&
 rm -rf dist;
-git add . &&
+git add &&
+git status && 
 git commit -m 'update' &&
-git push 
+git push
